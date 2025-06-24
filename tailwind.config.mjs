7672c46c8d7fd16c317dadf2ca4599086964aade
Bamzy123 <stephenomotos@gmail.com>
2025-06-24 11:49:25 +0100

    Navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import ('tailwindcss').config} */
+export default {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+        colors: {
+            lightHover: "#fcf4ff",
+            darkHover: "#2a004a",
+            darkTheme: "#11001f",
+        },
+        fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+            ovo: ["Ovo", "serif"],
+        },
+        },
+    },
+    plugins: [],
+}
