8bd06ffe763812aa3f649260a271c0e694be147d
Bamzy123 <stephenomotos@gmail.com>
2025-06-25 22:38:09 +0100

    Navbar Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,16 +7,16 @@ export default {
     ],
     theme: {
         extend: {
-        colors: {
-            lightHover: "#fcf4ff",
-            darkHover: "#2a004a",
-            darkTheme: "#11001f",
-        },
-        fontFamily: {
-            outfit: ["Outfit", "sans-serif"],
-            ovo: ["Ovo", "serif"],
-        },
+            colors: {
+                lightHover: "#fcf4ff",
+                darkHover: "#2a004a",
+                darkTheme: "#11001f",
+            },
+            fontFamily: {
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            }
         },
     },
     plugins: [],
-}
+};
